f89cbf61a6ba24bb2d9f404e7e26d4aa63e79d39
Austin Barrett <austinjb00@gmail.com>
2021-05-11 12:24:12 -0700

    Adding extra activities

--- app/src/main/java/edu/wwu/csci412/das_management_tracker/MainActivity.java
+++ app/src/main/java/edu/wwu/csci412/das_management_tracker/MainActivity.java
@@ -2,6 +2,7 @@ package edu.wwu.csci412.das_management_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread welcome = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(3000);
+                } catch (Exception error) {
+
+                } finally {
+
+                    Intent intent = new Intent(MainActivity.this, PinActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        welcome.start();
     }
 }
---
+++ app/src/main/java/edu/wwu/csci412/das_management_tracker/PinActivity.java
@@ -0,0 +1,6 @@
+package edu.wwu.csci412.das_management_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PinActivity extends AppCompatActivity {
+}
