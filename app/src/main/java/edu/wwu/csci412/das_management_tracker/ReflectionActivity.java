c4675a33c86608f5703d2d62fd3fd79feb8f4dd9
nemethj2 <nemethj2@wwu.edu>
2021-05-19 00:12:20 -0700

    base diary entry

--- app/src/main/java/edu/wwu/csci412/das_management_tracker/ReflectionActivity.java
+++ app/src/main/java/edu/wwu/csci412/das_management_tracker/ReflectionActivity.java
@@ -4,22 +4,31 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ReflectionActivity extends AppCompatActivity {
 
     ConstraintLayout reflectionLayout;
+    Toolbar toolbar;
+    RecyclerView recyclerView;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reflection);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        recyclerView = findViewById(R.id.diaryList);
 
         reflectionLayout = (ConstraintLayout) findViewById(R.id.layout_reflection);
 
@@ -33,5 +42,12 @@ public class ReflectionActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
